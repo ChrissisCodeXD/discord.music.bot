868a8ebe3c2bdcd38f3849a8428438b5323b5376
Chrissi <schwarzlichtampel@gmail.com>
2022-07-12 17:05:57 +0200

    playlists.py: added playlist cog for future impl

---
+++ musikbot/cogs/playlists.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+class PlaylistCog(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(bot)
+
